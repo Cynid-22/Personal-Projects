db574e0a7f34785297f22cf83a421ba3b8b970cf
Cynid-22 <vunguyenkhang@pm.me>
2025-06-20 13:07:32 +0700

    some-new-features

--- C++/SHA 256/Sha256.cpp
+++ C++/SHA 256/Sha256.cpp
@@ -138,7 +138,7 @@ string bitsetToHex(const std::bitset<256>& bits)
     return idkWhatHappenedButSomehowItEndedUpReversed;
 }
 
-int main()
+void sha256(string strMessage)
 {
     uint32_t H[8] =  {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
     uint32_t K[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -150,13 +150,11 @@ int main()
                     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
                     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
     
-    string strMessage;
     vector<bool> binMessage, hash;
     size_t messageBitLength = 0;
     bitset<256> digest;
 
-    cout << "Input message: ";
-    getline(cin, strMessage);
+    
 
     binMessage = TextToBinaryString(strMessage, messageBitLength);
     preProcessing(messageBitLength, binMessage);
@@ -167,6 +165,18 @@ int main()
     }
 
     cout << bitsetToHex(digest);
+}
+
+int main()
+{
+    string input;
+    cout << "Input message: ";
+    getline(cin, input);
+    while (input != "") {
+        sha256(input);
+        cout << "\nInput message (leave empty + enter to exit): ";
+        getline(cin, input);
+    }
 
     return 0;
 }
