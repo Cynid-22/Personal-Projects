afbca6fcd245deed689e16038d7cc1c44506cf67
Cynid-22 <vunguyenkhang@pm.me>
2025-06-25 13:03:07 +0700

    test

--- Cpp/test.cpp
+++ Cpp/test.cpp
@@ -2,9 +2,5 @@
 using namespace std;
 
 int main() {
-    int a = 5;
-    int b = 0;
-    cout << "Before division" << endl;
-    cout << a / b << endl; // Error here
-    return 0;
+    cout << "Hello World";
 }
