3befe2a90bb4532265ce8714497c33da42036082
Cynid-22 <vunguyenkhang@pm.me>
2025-06-25 09:26:43 +0700

    arbitrary-precision

--- C++/Word-average-length/average.cpp
+++ C++/Word-average-length/average.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <mpfr.h>
 
 using namespace std;
 
@@ -23,13 +24,37 @@ int main() {
     if (wordCount == 0) {
         cout << "No words found in file." << endl;
     } else {
-        __float128 average = static_cast<__float128>(totalLength) / wordCount;
-        cout << "Average word length: " << fixed << setprecision(20) << average << endl;
+        // Initialize MPFR numbers with 256-bit precision
+        mpfr_t totalLenMPFR, wordCountMPFR, average;
+        mpfr_init2(totalLenMPFR, 256);
+        mpfr_init2(wordCountMPFR, 256);
+        mpfr_init2(average, 256);
+
+        // Set values
+        mpfr_set_si(totalLenMPFR, totalLength, MPFR_RNDN);   // from int
+        mpfr_set_si(wordCountMPFR, wordCount, MPFR_RNDN);
+
+        // average = totalLen / wordCount
+        mpfr_div(average, totalLenMPFR, wordCountMPFR, MPFR_RNDN);
+
+        // Print result with 50 digits
+        mpfr_printf("Average word length: %.50Rf\n", average);
+
+        // Clean up
+        mpfr_clear(totalLenMPFR);
+        mpfr_clear(wordCountMPFR);
+        mpfr_clear(average);
+
+        // long double average = static_cast<long double>(totalLength) / wordCount;
+        // cout << "Average word length: " << fixed << setprecision(50) << average << endl;
+
+        // double average = static_cast<double>(totalLength) / wordCount;
+        // cout << "Average word length: " << fixed << setprecision(50) << average << endl;
     }
 
     return 0;
 }
 
 /*
-Average word length: 5.92279
+Average word length: 5.92279431924110325163129900751220047156878872621593
 */
