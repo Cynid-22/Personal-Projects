f467af438bcaf098ea4afe532f03e7c370101650
Cynid-22 <vunguyenkhang@pm.me>
2025-06-25 10:30:12 +0700

    rename

---
+++ Cpp/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 0;
+    cout << "Before division" << endl;
+    cout << a / b << endl; // Error here
+    return 0;
+}
